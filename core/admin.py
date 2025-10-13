8ec14d492ea4b4fe163bfbbe2e31af08a96fd66f
Stevetatu <stevetatu3@gmail.com>
2025-10-14 01:54:48 +0300

    Admin core models registration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserPantry, Ingredient, Recipe, ShoppingList, ConsumptionRecord, FoodWasteRecord, ShoppingListItem, ImageProcessingJob 
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(UserPantry)
+admin.site.register(Recipe)
+admin.site.register(ShoppingList)
+admin.site.register(ShoppingListItem)
+admin.site.register(ConsumptionRecord)
+admin.site.register(FoodWasteRecord)
+admin.site.register(ImageProcessingJob)
+
